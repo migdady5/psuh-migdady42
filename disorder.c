34d6b8c9964630d24fc0c87ba31131f8f90f0711
Toqa Meqdad <toqamok2005@gmail.com>
2026-02-09 17:47:14 +0300

    compute disorder

--- disorder.c
+++ disorder.c
@@ -1,18 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pushswap.h                                         :+:      :+:    :+:   */
+/*   disorder.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmeqdad <toqa.meqdad@learner.42.tech>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/30 16:06:07 by tmeqdad           #+#    #+#             */
-/*   Updated: 2026/01/30 16:06:07 by tmeqdad          ###   ########.fr       */
+/*   Created: 2026/02/09 16:39:23 by tmeqdad           #+#    #+#             */
+/*   Updated: 2026/02/09 16:39:23 by tmeqdad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-double	compute_disorder(t_node *a);
-typedef struct s_node
+#include "pushswap.h"
+
+double	compute_disorder(t_node *a)
 {
-	void			*data;
-	struct s_node	*next;
-}	t_node;
+	t_node	*i;
+	t_node	*j;
+	double	mistakes;
+	double	total_pairs;
+
+	mistakes = 0.0;
+	total_pairs = 0.0;
+	i = a;
+	while (i)
+	{
+		j = i->next;
+		while (j)
+		{
+			total_pairs++;
+			if (i->value > j->value)
+				mistakes++;
+			j = j->next;
+		}
+		i = i->next;
+	}
+	if (total_pairs == 0)
+		return (0.0);
+	return (mistakes / total_pairs);
+}
